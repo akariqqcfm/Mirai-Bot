167ebfb1f9adb0fda0ecb17b08b853287ea61b05
akariqqcfm <63291581+akariqqcfm@users.noreply.github.com>
2020-04-08 00:55:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Create by AkariM - HerokuGithub!');
 });
 
 client.on('message', message => {
